8fbd56641399625b6e28269cb999b3b0588a8bf3
Amila Welihinda <amilajack@gmail.com>
2018-12-09 14:25:00 -0800

    format with rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-use std::vec::Vec;
 use num::Num;
-use std::fmt::{Display, Formatter, Error, Debug};
-use std::cmp::{Ordering};
+use std::cmp::Ordering;
+use std::fmt::{Debug, Display, Error, Formatter};
 use std::mem;
+use std::vec::Vec;
 
 pub use self::ObjectiveKind::*;
 
@@ -36,7 +36,7 @@ pub struct Rect {
 pub struct Matrix<F: Num + PartialEq> {
     pub h: usize,
     pub w: usize,
-    pub m: Vec<F> // Size is (at least) w * h
+    pub m: Vec<F>, // Size is (at least) w * h
 }
 
 impl<F: Num + Copy + PartialEq> Matrix<F> {
@@ -46,25 +46,23 @@ impl<F: Num + Copy + PartialEq> Matrix<F> {
         unsafe { *self.m.get_unchecked(j + self.w * i) }
     }
 
-    pub fn set_at(&mut self, i: usize, j:usize, x: F) {
+    pub fn set_at(&mut self, i: usize, j: usize, x: F) {
         // assert!(i < self.h);
         // assert!(j < self.w);
-        unsafe { *self.m.get_unchecked_mut(j + self.w * i) = x; }
+        unsafe {
+            *self.m.get_unchecked_mut(j + self.w * i) = x;
+        }
     }
 
     pub unsafe fn allocate_mem(h: usize, w: usize) -> Matrix<F> {
         let mut m: Vec<F> = Vec::with_capacity(h * w);
         m.set_len(h * w);
-        Matrix {
-            h: h,
-            w: w,
-            m: m,
-        }
+        Matrix { h: h, w: w, m: m }
     }
 
     pub fn allocate_zeroed(h: usize, w: usize) -> Matrix<F> {
         let mut m = unsafe { Self::allocate_mem(h, w) };
-        for i in 0..h*w {
+        for i in 0..h * w {
             m.m[i] = F::zero();
         }
         m
@@ -74,7 +72,7 @@ impl<F: Num + Copy + PartialEq> Matrix<F> {
         for i in 0..src.h {
             for j in 0..src.w {
                 let val = self.at(src.i + i, src.j + j);
-                dst.set_at(i_dst+i, j_dst+j, val);
+                dst.set_at(i_dst + i, j_dst + j, val);
             }
         }
     }
@@ -88,8 +86,8 @@ pub struct Dictionary<F: OrdField> {
     pub m: Matrix<F>,
     pub ll: Vec<usize>, // lines labels
     pub lc: Vec<usize>, // cols labels
-    pub obj: Vec<F>, // We maximize
-    pub weq: Vec<F>,  // working equation
+    pub obj: Vec<F>,    // We maximize
+    pub weq: Vec<F>,    // working equation
     pub var_name: &'static str,
 }
 
@@ -102,7 +100,7 @@ pub enum Heuristic {
 #[derive(PartialEq, Debug)]
 pub enum LeavingCase<F: PartialOrd> {
     NonNeg, // +infty
-    Pos(usize, F)
+    Pos(usize, F),
 }
 
 const FIRST_PHASE_IDX: usize = 1 << 30;
@@ -110,18 +108,18 @@ const FIRST_PHASE_IDX: usize = 1 << 30;
 impl<F: PartialOrd + Copy> PartialOrd for LeavingCase<F> {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         use self::LeavingCase::*;
-        use std::cmp::Ordering::{Less, Greater};
+        use std::cmp::Ordering::{Greater, Less};
         match (self, other) {
             (&NonNeg, _) => Some(Greater),
-            (&Pos(_, x1), &Pos(_, x2)) => Some (if x1 < x2 { Less } else { Greater }),
-            _ => Some(Less)
+            (&Pos(_, x1), &Pos(_, x2)) => Some(if x1 < x2 { Less } else { Greater }),
+            _ => Some(Less),
         }
     }
 }
 
 pub enum Step {
     Finished,
-    Unbounded(usize), // "entering variable"
+    Unbounded(usize),       // "entering variable"
     Continue(usize, usize), // (entering, leaving)
 }
 
@@ -142,49 +140,98 @@ impl<F: OrdField> Dictionary<F> {
         self.m.h
     }
 
-
     fn create_first_dict(&self) -> Dictionary<F> {
-       let mut m = Matrix::allocate_zeroed(self.h(), self.w() + 1);
-       self.m.blit(&mut m, Rect {i: 0, j: 0, h: self.h(), w: self.w()}, 0, 0);
-       for i in 0..self.h() {
-           m.set_at(i, self.w(), F::one());
-       }
-       let mut lc = self.lc.clone();
-       lc.push(FIRST_PHASE_IDX);
-       let mut obj = init_zero_vec(m.w -1, F::zero());
-       obj.push(F::zero() - F::one());
-       let res = Dictionary {
-           m: m,
-           ll: self.ll.clone(),
-           lc: lc,
-           obj: obj,
-           weq: init_zero_vec(self.w()+1, F::zero()),
-           var_name: self.var_name,
-       };
-       res.check_integrity();
-       res
+        let mut m = Matrix::allocate_zeroed(self.h(), self.w() + 1);
+        self.m.blit(
+            &mut m,
+            Rect {
+                i: 0,
+                j: 0,
+                h: self.h(),
+                w: self.w(),
+            },
+            0,
+            0,
+        );
+        for i in 0..self.h() {
+            m.set_at(i, self.w(), F::one());
+        }
+        let mut lc = self.lc.clone();
+        lc.push(FIRST_PHASE_IDX);
+        let mut obj = init_zero_vec(m.w - 1, F::zero());
+        obj.push(F::zero() - F::one());
+        let res = Dictionary {
+            m: m,
+            ll: self.ll.clone(),
+            lc: lc,
+            obj: obj,
+            weq: init_zero_vec(self.w() + 1, F::zero()),
+            var_name: self.var_name,
+        };
+        res.check_integrity();
+        res
     }
 
     /// Creates a new `Dictionary` without the component `xr`
     /// It creates a copy of itself if xr is not a variable of `self`
     fn project_dict(&self, xr: usize, orig: &mut Dictionary<F>) {
         if let Some(ir) = self.ll.iter().position(|&x| x == xr) {
-            self.m.blit(&mut orig.m, Rect{i: 0, j: 0, h: ir, w: self.w()}, 0, 0);
-            self.m.blit(&mut orig.m, Rect{i: ir+1, j: 0, h: self.h()-ir-1, w: self.w()}, ir, 0);
+            self.m.blit(
+                &mut orig.m,
+                Rect {
+                    i: 0,
+                    j: 0,
+                    h: ir,
+                    w: self.w(),
+                },
+                0,
+                0,
+            );
+            self.m.blit(
+                &mut orig.m,
+                Rect {
+                    i: ir + 1,
+                    j: 0,
+                    h: self.h() - ir - 1,
+                    w: self.w(),
+                },
+                ir,
+                0,
+            );
             orig.fix_obj_after_first_phase(self, FIRST_PHASE_IDX);
             let mut ll = self.ll.clone();
-            ll.remove(ir); // TODO(leo): optimize?
-            orig.ll= ll;
+            ll.remove(ir); // TODO: optimize?
+            orig.ll = ll;
             orig.lc = self.lc.clone();
         } else if let Some(jr) = self.lc.iter().position(|&x| x == xr) {
-            self.m.blit(&mut orig.m, Rect{i: 0, j: 0, h: self.h(), w: jr}, 0, 0);
-            self.m.blit(&mut orig.m, Rect{i: 0, j: jr+1, h: self.h(), w: self.w()-jr-1}, 0, jr);
+            self.m.blit(
+                &mut orig.m,
+                Rect {
+                    i: 0,
+                    j: 0,
+                    h: self.h(),
+                    w: jr,
+                },
+                0,
+                0,
+            );
+            self.m.blit(
+                &mut orig.m,
+                Rect {
+                    i: 0,
+                    j: jr + 1,
+                    h: self.h(),
+                    w: self.w() - jr - 1,
+                },
+                0,
+                jr,
+            );
             orig.fix_obj_after_first_phase(self, FIRST_PHASE_IDX);
             let mut obj = self.obj.clone();
             let mut lc = self.lc.clone();
             obj.remove(jr);
             lc.remove(jr);
-            orig.ll= self.ll.clone();
+            orig.ll = self.ll.clone();
             orig.lc = lc;
         }
     }
@@ -194,9 +241,12 @@ impl<F: OrdField> Dictionary<F> {
         res[0] = self.obj[0];
         for (j, &c_j) in self.obj.iter().enumerate() {
             let x_j = self.lc[j];
-            if x_j == exc { continue; }
+            if x_j == exc {
+                continue;
+            }
             if c_j != F::zero() {
-                if let Some(&x_i) = afp.ll.iter().find(|&x| *x == x_j) { // TODO(leo): WTF???
+                if let Some(&x_i) = afp.ll.iter().find(|&x| *x == x_j) {
+                    // TODO: WTF???
                     // x_i is a primary variable of afp.
                     for j in 0..res.len() {
                         res[j] = res[j] + c_j * self.m.at(x_i, j); // I FIXME(leo): i hate myself
@@ -220,21 +270,26 @@ impl<F: OrdField> Dictionary<F> {
         res
     }
 
-    pub fn find_leaving_variable(&self, je: usize) -> LeavingCase<F> { // TODO(leo): get rid of dumb LeavingCase :/
+    pub fn find_leaving_variable(&self, je: usize) -> LeavingCase<F> {
+        // TODO: get rid of dumb LeavingCase :/
         use self::LeavingCase::*;
         //assert!(je != 0);
-        let coeffs = (0..self.h()).map(|i| if self.m.at(i, je) < F::zero() {
-            Pos(i, F::zero() - self.m.at(i, 0) / self.m.at(i, je))
-        } else {
-            NonNeg
+        let coeffs = (0..self.h()).map(|i| {
+            if self.m.at(i, je) < F::zero() {
+                Pos(i, F::zero() - self.m.at(i, 0) / self.m.at(i, je))
+            } else {
+                NonNeg
+            }
         });
 
         let res = coeffs.fold(None, |min, x| match min {
             None => Some(x),
-            Some(m) => if x < m {
-                Some(x)
-            } else {
-                Some(m)
+            Some(m) => {
+                if x < m {
+                    Some(x)
+                } else {
+                    Some(m)
+                }
             }
         });
 
@@ -244,7 +299,7 @@ impl<F: OrdField> Dictionary<F> {
     fn eval_line(&self, sol: &[F], i: usize) -> F {
         let mut sum = self.m.at(i, 0);
         for j in 1..self.w() {
-            sum = sum + self.m.at(i, j) * sol[j-1];
+            sum = sum + self.m.at(i, j) * sol[j - 1];
         }
         sum
     }
@@ -259,12 +314,13 @@ impl<F: OrdField> Dictionary<F> {
         true
     }
 
-    pub fn find_entering_variable(&self) -> Step { //TODO(leo): handle all cases
+    pub fn find_entering_variable(&self) -> Step {
+        //TODO: handle all cases
         use self::Step::*;
         for j in 1..self.w() {
             if self.obj[j] > F::zero() {
                 if let LeavingCase::Pos(i, _) = self.find_leaving_variable(j) {
-                    return Continue(i, j)
+                    return Continue(i, j);
                 } else {
                     return Unbounded(j);
                 }
@@ -278,14 +334,17 @@ impl<F: OrdField> Dictionary<F> {
         let mut max = self.obj[1];
         let mut jmax = 1;
         for (j, &c_j) in self.obj[1..].iter().enumerate() {
-            if c_j >= max { max = c_j; jmax = j; }
+            if c_j >= max {
+                max = c_j;
+                jmax = j;
+            }
         }
 
         let j = jmax + 1;
 
         if max > F::zero() {
             if let LeavingCase::Pos(i, _) = self.find_leaving_variable(j) {
-                return Continue(i, j)
+                return Continue(i, j);
             } else {
                 return Unbounded(j);
             }
@@ -295,7 +354,7 @@ impl<F: OrdField> Dictionary<F> {
 
     pub fn run_simplex(&mut self, heur: Heuristic, latex: bool) -> Option<F> {
         let do_first_phase = {
-            let nil_sol: Vec<F> = init_zero_vec(self.w()-1, F::zero());
+            let nil_sol: Vec<F> = init_zero_vec(self.w() - 1, F::zero());
             !self.is_solution(nil_sol)
         };
 
@@ -324,13 +383,12 @@ impl<F: OrdField> Dictionary<F> {
                 return None;
             }
 
-
             //println!("END OF FIRST PHASE with res {}", d.obj[0]);
             d.project_dict(FIRST_PHASE_IDX, self);
             //println!("DICT FOR BEGINNING OF SECOND PHASE:\n {}", self);
         }
 
-        let fev: fn (&Self) -> Step = match heur {
+        let fev: fn(&Self) -> Step = match heur {
             Heuristic::Bland => Self::find_entering_variable,
             Heuristic::Dumb => Self::find_entering_variable_dumb,
         };
@@ -342,20 +400,23 @@ impl<F: OrdField> Dictionary<F> {
             match fev(self) {
                 Step::Continue(i, j) => {
                     if latex {
-                        println!("Performing pivot: entering {}, leaving {}\n", self.lc[j], self.ll[i]);
+                        println!(
+                            "Performing pivot: entering {}, leaving {}\n",
+                            self.lc[j], self.ll[i]
+                        );
                     }
                     self.perform_pivot(j, i)
-                },
-                Step::Unbounded(_) => { // TODO: give infinite line
+                }
+                Step::Unbounded(_) => {
+                    // TODO: give infinite line
                     println!("This LP is unbounded!");
                     return None;
-                },
+                }
                 Step::Finished => break,
             }
         }
 
         return Some(self.obj[0]);
-
     }
 
     /// `je`: entering variable
@@ -381,9 +442,9 @@ impl<F: OrdField> Dictionary<F> {
                 for j in 0..self.w() {
                     let old = self.m.at(i, j);
                     if j != je {
-                        self.m.set_at(i, j, old + a*vec_at(&self.weq, j));
+                        self.m.set_at(i, j, old + a * vec_at(&self.weq, j));
                     } else {
-                        self.m.set_at(i, j, a*vec_at(&self.weq, j));
+                        self.m.set_at(i, j, a * vec_at(&self.weq, j));
                     }
                 }
             } else {
@@ -397,10 +458,9 @@ impl<F: OrdField> Dictionary<F> {
             //assert!(a != F::zero());
             let old = vec_at(&self.obj, j);
             if j != je {
-                self.obj[j] = old + a*vec_at(&self.weq, j);
+                self.obj[j] = old + a * vec_at(&self.weq, j);
             } else {
-                self.obj[j] = a*vec_at(&self.weq, j);
-
+                self.obj[j] = a * vec_at(&self.weq, j);
             }
         }
 
@@ -410,22 +470,28 @@ impl<F: OrdField> Dictionary<F> {
 }
 
 impl<F: OrdField> Display for Dictionary<F> {
-    // TODO(leo): Print x_0 as a cte
+    // TODO: Print x_0 as a cte
     fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
         write!(f, r"$$ \begin{{array}}{{ccccccccccccccc}}");
         for i in 0..self.h() {
             let _ = write!(f, "&{}_{{ {} }} &= ", self.var_name, self.ll[i]);
 
             let mut first = true;
-            for j in 0..self.w() { // TODO(leo): ugly!
+            for j in 0..self.w() {
+                // TODO: ugly!
                 if !first {
                     let _ = write!(f, "& + ");
                 }
                 first = false;
-                let _ = write!(f, "&{} {}_{{ {} }}", self.m.at(i, j), self.var_name, self.lc[j]);
+                let _ = write!(
+                    f,
+                    "&{} {}_{{ {} }}",
+                    self.m.at(i, j),
+                    self.var_name,
+                    self.lc[j]
+                );
             }
             let _ = write!(f, "\\\\ \n");
-
         }
         let _ = write!(f, "&z   &= ");
         let mut first = true;
@@ -470,7 +536,7 @@ pub struct NormalLinearProgram<F: OrdField> {
     ineqs: Vec<Inequation<F>>,
     obj: Vec<F>,
     obj_kind: ObjectiveKind,
-    // TODO(leo): names
+    // TODO: names
 }
 
 impl<F: OrdField> NormalLinearProgram<F> {
@@ -486,7 +552,7 @@ pub fn make_dict() -> Dictionary<f64> {
         m: Matrix {
             h: 2,
             w: 3,
-            m: vec![8., 1., 2., 12.,  -3., -4.]
+            m: vec![8., 1., 2., 12., -3., -4.],
         },
         ll: vec![3, 4],
         lc: vec![0, 1, 2],
@@ -517,7 +583,7 @@ mod test {
     #[test]
     fn test_leaving_variable() {
         let lp = make_dict();
-        assert_eq!(lp.find_leaving_variable(1), super::LeavingCase::Pos(1, 4.0)) // TODO(leo): correct??
+        assert_eq!(lp.find_leaving_variable(1), super::LeavingCase::Pos(1, 4.0)) // TODO: correct??
     }
 
     #[test]
